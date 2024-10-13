9f2c371daaeecec92d464e0332ba07bc6919ed9a
Only Up <76998342+Only-Up-Offical@users.noreply.github.com>
2024-10-13 11:56:02 +0400

    Update caesar.cpp
    Added function that cracks Caesar Cipher if user does not know the shift value

--- caesar.cpp
+++ caesar.cpp
@@ -39,6 +39,16 @@ string decrypt(const string& text, int shift){
     return decrypted_text;
 }
 
+void crackcipher(const string& text){
+    setcolor(4);
+    cout << "Trying all possible shift values:\n";
+    setcolor(2);
+    for(int i = 1; i < 26; i++){
+        cout << "Shift " << i << ": " << decrypt(text, i) << endl;
+    }
+    setcolor(7);
+}
+
 int main(){
     string caesar_cipher = R"(
    ___                          ___ _      _            
@@ -54,8 +64,8 @@ int main(){
     int shift = 3; // default shift is 3
     string msg;
 
-    cout << "\n\n1.Encrypt text\n2.Decrypt text\n";
-    cout << "Choose option 1 or 2: ";
+    cout << "\n\n1.Encrypt text\n2.Decrypt text\n3.Crack Caesar Cipher\n";
+    cout << "Choose option 1,2 or 3: ";
     int opt; cin >> opt;
     cin.ignore();
 
@@ -66,11 +76,15 @@ int main(){
         setcolor(2);
         cout << "\nEncrypted text: ";setcolor(7);cout << encrypt(msg, shift);
 
-    } else{
+    } else if(opt == 2){
         cout << "Enter text to be decrypted: ";
         getline(cin, msg);
         cout << "Enter the shift value: "; cin >> shift;
         setcolor(2);
         cout << "\nDecrypted text: ";setcolor(7);cout << decrypt(msg, shift);
+    } else if(opt == 3){
+        cout << "Enter the encrypted text to crack: ";
+        getline(cin, msg);
+        crackcipher(msg);
     }
 }
